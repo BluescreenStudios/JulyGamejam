ee0da86642e50a94684a8bd96740f380991ca1e4
BluescreenStudios <bluescreenisagame@gmail.com>
2024-07-30 21:28:59 -0500

    Main menu

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -7,9 +7,9 @@ func _on_boot_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
-func _on_settings_pressed():
-	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
+func _on_levels_pressed():
+	get_tree().change_scene_to_file("res://Scenes/level_select.tscn")
 
 
-func _on_shut_down_pressed():
+func _on_power_off_pressed():
 	get_tree().quit()
